5518aac24b370e09e8cdb20b33aca28a6f2cc453
Rajashekar <rajashekarreddy339@gmail.com>
2018-12-22 10:21:59 +0530

    first commit in JS

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+console.log("welcome to JS")
+//document.write("Welcome to JavaScript");
+
+/*var txt = "Welcome \'Raj'\ ";
+document.write(txt);
+txt.length;
+
+var x = "John"
+var y = new String("Raj")
+
+document.write(x + " " +y)
+
+var car = {name:'swift' , model:'2013', engineCc:'1200'};
+document.write(car.name + " bought in year" + car.model + "with:" + car.engineCc + "cc")
+*/
+
+function clickMe(x,y){ 
+document.write(x*y)
+}
